f0810fb5a63834c0a45265965247f791723e8252
Ariel Gispan <ariel@nrgene.com>
2019-11-24 17:16:16 +0200

    new report with ipython

--- src/redshift_queries/redshift_basic.py
+++ src/redshift_queries/redshift_basic.py
@@ -10,3 +10,23 @@ def createConnectionCursor(host):
     conn = psycopg2.connect("dbname={} user={} host={} port={} password={}".format(dbname,user,host,port,password))
     cur = conn.cursor()
     return cur
+
+def get_all_results(host, query):
+    cur = createConnectionCursor(host)
+    cur.execute(query)
+    rows = cur.fetchall()
+    cur.close()
+    return rows
+
+
+def get_table_name(host, data_version, table_name):
+    query = 'select table_name from {}_data_version where table_type = \'{}\';'.format(data_version, table_name)
+    cur = createConnectionCursor(host)
+    cur.execute(query)
+    row = cur.fetchone()
+    cur.close()
+    return row
+
+
+
+
