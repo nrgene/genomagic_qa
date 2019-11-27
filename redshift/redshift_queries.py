880f6eac0b7771c7dd03fff5fb88c4d14921b3d5
Ariel Gispan <ariel@nrgene.com>
2019-11-27 17:08:29 +0200

    add *

--- redshift/redshift_queries.py
+++ redshift/redshift_queries.py
@@ -1,8 +1,11 @@
 import psycopg2
 import os
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-def createConnectionCursor(host):
+
+def create_connection_cursor(host):
     dbname='genomagic'
     user='nrgene'
     password=os.getenv("PGPASSWORD")
@@ -15,7 +18,7 @@ def createConnectionCursor(host):
 
 def get_all_results(host, query):
     assert query[-1] == ';'
-    cur = createConnectionCursor(host)
+    cur = create_connection_cursor(host)
     cur.execute(query)
     rows = cur.fetchall()
     cur.close()
@@ -81,22 +84,65 @@ def get_hap_info(cur, table_name, my_filter):
     print(rows)
 
 
-def get_samples_type_info(host, data_version):
+def get_samples_type_info_as_string(host, data_version):
     samples_table = get_table_name(host, data_version, 'SAMPLES')
     rows = simple_grouping(host, samples_table, 'analysis_method')
     s = ''
     for r in rows:
         assert len(r) == 2
-        print('{} samples count = {}'.format(r[1], r[0]))
+        s = '{}\n{} samples count = {}'.format(s,r[1], r[0])
+    return s
 
-def
-haplotypes_info_table = rs.get_table_name(host, data_version, 'HAPLOTYPES_INFO')
-rows = rs.get_all_results(host, 'SELECT COUNT(*) FROM {};'.format(haplotypes_info_table))
-hap_markers_count = int(rows[0][0]/1000000)
-rows = rs.get_all_results(host, 'SELECT COUNT(*) FROM {} WHERE chromosome=0;'.format(haplotypes_info_table))
-unmapped_hap_markers_count = int(rows[0][0]/1000000)
-print('in table {} there are total of {}M haplotype markers {}M of them are unmapped'.format(haplotypes_info_table, hap_markers_count, unmapped_hap_markers_count))
 
+def get_hap_count_total_hap_markers_as_string(host, data_version):
+    haplotypes_info_table = get_table_name(host, data_version, 'HAPLOTYPES_INFO')
+    rows = get_all_results(host, 'SELECT COUNT(*) FROM {};'.format(haplotypes_info_table))
+    hap_markers_count = int(rows[0][0]/1000000)
+    rows = get_all_results(host, 'SELECT COUNT(*) FROM {} WHERE chromosome=0;'.format(haplotypes_info_table))
+    unmapped_hap_markers_count = int(rows[0][0]/1000000)
+    return 'in table {} there are total of {}M haplotype markers {}M of them are unmapped'.format(haplotypes_info_table, hap_markers_count, unmapped_hap_markers_count)
+
+
+def get_hap_samples_total_as_string(host, data_version):
+    haplotypes_samples_table = get_table_name(host, data_version, 'HAPLOTYPE_SAMPLES')
+    sub_table_samples = 'samples AS (SELECT haplotype_idx, sample_id FROM {})'.format(haplotypes_samples_table)
+    haplotypes_info_table = get_table_name(host, data_version, 'HAPLOTYPES_INFO')
+    sub_table_haps = 'haps AS (SELECT haplotype_idx, chromosome FROM {})'.format(haplotypes_info_table)
+    inner_join_sub_table = 'samples INNER JOIN haps ON samples.haplotype_idx=haps.haplotype_idx'
+    hapsXsamples_query = 'WITH {}, {} SELECT COUNT(*) FROM {}'.format(sub_table_samples, sub_table_haps,
+                                                                      inner_join_sub_table)
+    rows = get_all_results(host, '{};'.format(hapsXsamples_query))
+    total_haps_samples = int(rows[0][0] / 1000000)
+    rows = get_all_results(host, '{} WHERE chromosome=0;'.format(hapsXsamples_query))
+    unmapped_haps_samples = int(rows[0][0] / 1000000)
+    print('There are {}M hapsXsamples {}M of them are unmapped'.format(total_haps_samples, unmapped_haps_samples))
+
+
+def hist_count(host, data_version):
+    similarity_table = get_table_name(host, data_version, 'HAPLOTYPES_SIMILARITY')
+    score_query = 'SELECT COUNT(*), ROUND(similarity_score,1) as sc from {} GROUP BY sc;'.format(similarity_table)
+
+    rows = get_all_results(host, score_query)
+    assert len(rows) == 11
+    df = pd.DataFrame.from_records(rows, columns=['Count', 'Score']).sort_values(by=['Score'])
+    height = df['Count']
+    bars = df['Score']
+    y_pos = np.arange(len(bars))
+    plt.bar(y_pos, height)
+    plt.xticks(y_pos, bars)
+    plt.show()
+
+
+def hist_count2(host, data_version):
+    similarity_table = get_table_name(host, data_version, 'HAPLOTYPES_SIMILARITY')
+    score_len_query = 'SELECT AVG(end_position-start_position), ROUND(similarity_score,2) as sc from {} GROUP BY sc;'.format(similarity_table)
+    rows = get_all_results(host, score_len_query)
+    df = pd.DataFrame.from_records(rows, columns =['mean len', 'score']).sort_values(by=['score'])
+    plt.scatter(df['score'], df['mean len'])
+    plt.title('score vs len')
+    plt.xlabel('score')
+    plt.ylabel('len')
+    plt.show()
 
 
 
