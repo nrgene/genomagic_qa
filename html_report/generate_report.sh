127b6422803462e52e3590d005dcc6e6c24b4c0f
Ariel Gispan <ariel@nrgene.com>
2019-12-02 16:00:21 +0200

    add host as parameter

--- html_report/generate_report.sh
+++ html_report/generate_report.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 my_dir=$(dirname "$0")
 my_data_version=$1
+my_rs_host=$2
 my_notebook=$my_dir/data_version_report.ipynb
 export DATA_VERSION=$my_data_version
+export REDSHIFT_HOST=$my_rs_host
 jupyter nbconvert $my_notebook --TemplateExporter.exclude_input=True --ExecutePreprocessor.enabled=True --ExecutePreprocessor.allow_errors=True --ExecutePreprocessor.timeout=180 --output-dir=./ --to html
 
 
