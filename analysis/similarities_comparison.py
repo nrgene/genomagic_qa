26380ac12fdf67a97fe202e13e9f9a18ac7826c7
Ariel Gispan <ariel@nrgene.com>
2020-02-17 09:18:00 +0200

    add usage script

--- analysis/similarities_comparison.py
+++ analysis/similarities_comparison.py
@@ -4,7 +4,7 @@ import pandas as pd
 working_dir_path = os.getcwd()
 genomagic_qa_repo_path = '/'.join(working_dir_path.split('/')[:-1])
 sys.path.append(genomagic_qa_repo_path)
-import api_server.requests as ar
+
 
 
 def advance_in_list(my_list, prev_element_end):
@@ -180,23 +180,7 @@ def compute_similarity_match_for_multiple_sampels_in_df(hap_sim1, hap_sim2, samp
     return [hap_sim1_uniq_len, hap_sim2_uniq_len, shared_len]
 
 
-#api_server = 'api-dev.nrgene.local:8080'
-#dv1='maize_benchmark_test_fix_mkrs_919_03'
-#dv2 = 'maize_benchmark_only_arg_wgs'
-#dv3 = 'dm_gm_public_maize_232'
-#samples_wgs=['b73v4__ver100', 'cml247__ver100', 'ep1_v2__ver100', 'w22__ver100', 'ki3__ver110', 'f7_v2__ver100', 'mo17__ver100']
-#samples_snp = ['b73', 'cml247', 'ep1', 'w22', 'ki3', 'f7', 'mo17']
-#n = len(samples_wgs)
-#my_dict = {}
-#for i in range(n):
-#    my_dict[samples_snp[i]] = samples_wgs[i]
-#hap_sim1 = ar.get_raw_similarities_between_multiple_sampels(api_server, dv1, samples_wgs, "1,2")
-#hap_sim2 = ar.get_raw_similarities_between_multiple_sampels(api_server, dv2, samples_wgs, "1,2")
-#snp_sim = ar.get_raw_similarities_between_multiple_sampels(api_server, dv3, samples_snp, "1,2").replace(my_dict)
-#[fn1, fp1, intersect1] = compute_similarity_match_for_multiple_sampels_in_df(hap_sim1, snp_sim, samples_wgs)
-#[fn2, fp2, intersect2] = compute_similarity_match_for_multiple_sampels_in_df(hap_sim2, snp_sim, samples_wgs)
-#print("{} {} {}".format(fn1/1000000, fp1/1000000, intersect1/1000000))
-#print("{} {} {}".format(fn2/1000000, fp2/1000000, intersect2/1000000))
+
 
 #[fn1, fp1, intersect1] = compute_similarity_match_for_multiple_sampels_in_df(hap_sim1, hap_sim2, samples)
 #print("{}, {}, {}".format(fn1/1000000, fp1/1000000, intersect1/1000000))
